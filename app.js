4b0cfbafaf6dc2fa0486875032774de4fd125a6e
Pablo <paulvaughan.dev@gmail.com>
2019-04-10 09:28:05 -0400

    Commit for adjustments to fine-tune busmall project. JS reconfig

--- app.js
+++ app.js
@@ -1,178 +1,136 @@
 "strict mode"
 
-let pictureArray = [];
+// declare empty array
+let selectionArray = []
 
-//declare variables which represent the  the four  categories  types for each page
-// let kitchenItemsArray = [];
-// let therapeuticsArray = [];
-// let noveltyItemsArray = [];
-// let sportsAttireArray = [];
-
-//find our image container using document.getElementById
-let elImageContainer = document.getElementById('imageContainer');
-//console.log(elImageContainer);
+let elImageContainer = document.getElementById('imageContainer')
+let elStartOver = document.getElementById('startOver')
 
 
-let ImageConstructor = function (name, filePath, category, id) {
-    this.name = name;
-    this.filePath = filePath;
-    this.category = category;
-    this.id = id;
-    this.clicked = 0;
-    this.shown = 0;
+let BusMall = function (name, filePath, id){
+  this.name = name;
+  this.filePath = filePath;
+  this.id = id;
+  this.selected = 0;
+  this.shown = 0
+}
+
+let totalSelected;
+
+if(localStorage.length > 0){
+  let getData = localStorage.getItem('BusMallStorage');
+  selectionArray = JSON.parse(getData);
+  let getMoreData = localStorage.getItem('TotalSelectedStorage');
+  totalSelected = parseInt(getMoreData)
+} else{
+  totalSelected = 0; //instantiating new reference to our images objects
+  let airMax = new BusMall('Nike Air Max', './assets/airmax.jpg', 'air max');
+  let airOne = new BusMall('Air Force One', './assets/airone.jpg', 'red air force');
+  let vespa = new BusMall('vespaBike', './assets/vesp.jpg', 'vintage vespa');
+  let sportsBra = new BusMall('Sports Bra', './assets/sportsbra.jpg', 'sports bra');
+  let baking = new BusMall('Baking items', './assets/baking.jpg', 'baking');
+  let brownAirForce = new BusMall('Brown Airforce One', './assets/brownairforce.jpg', 'brown Air');
+  let dogMask = new BusMall('Doggie disguise', './assets/dogdisguise.jpg', 'dog guise')
+  let eggHolder = new BusMall('Egg Holder', './assets/egholder.jpg', 'egg holder');
+  let hippie = new BusMall('Hippie', './assets/hippie.jpg', 'hippie');
+  let hipster = new BusMall('Hipster', './assets/hipster.jpg', 'hipster');
+  let lakersJersey = new BusMall('Laker Jersey', './assets/lakersjersey.jpg', 'laker jersey');
+  let mixBowl = new BusMall('Mixing Bowl', './assets/mixbowl.jpg', 'mixing bowl');
+  let nikeBall = new BusMall('Nike Basketball', './assets/nikeball.jpg', 'nike basketball');
+  let nikeBoots = new BusMall('Nike Boots', './assets/nikeboots.jpg', 'soccer boots');
+  let oldBike = new BusMall('Old Bike', './assets/oldbike.jpg', 'nike boots');
+  let oldRecords = new BusMall('Old Records', './assets/oldrecords.jpg', 'old records');
+  let oldTyper = new BusMall('Old Typewriter', './assets/oldtyper.jpg', 'old typer');
+  let polaroidCam = new BusMall('Polaroid Camera', './assets/polaroidcamera.jpg', 'polaroid camera');
+  let redKettle = new BusMall('Red Kettle', './assets/redkettle.jpg', 'red kettle');
+  let redVespa = new BusMall('Red Vespa', './assets/redvespa.jpg', 'redvesp');
+  let retroJordan = new BusMall('Retro Jordan', './assets/retrojordan.jpg', 'retro jordans');
+  let smokePipe = new BusMall('Smoking Pipe', './assets/smokingpipe.jpg', 'smoking pipe');
+  let spoons = new BusMall('Spoons', './assets/spoons.jpg', 'plastic spoons');
+  let teaCup = new BusMall('Tea Cup', './assets/teacup.jpg', 'fancy teacup');
+  let utensils = new BusMall('Utensils', './assets/utensils.jpg', 'kitchen utensils');
+  let vintageType = new BusMall('Vintage Typewriter', './assets/vintagetype.jpg', 'type writer');
+  let wok = new BusMall('Iron Wok', './assets/wok.jpg', 'iron wok');
+  let yellowMoped = new BusMall('Yellow Moped', './assets/yellowmoped.jpg', 'yellow vesp');
+  selectionArray.push(airMax, airOne, vespa, sportsBra, baking, brownAirForce, dogMask, eggHolder, hippie, hipster, lakersJersey, mixBowl, nikeBall, nikeBoots, oldBike, oldRecords, oldTyper, polaroidCam, redKettle, redVespa, retroJordan, smokePipe, spoons, teaCup, utensils, vintageType, wok, yellowMoped) 
+} //all pushed to fill the empty selectionArray
+
+// Create a function to shuffle/randomize the images in the array
+function shuffleImage(){
+  let randomNumber = Math.floor(Math.random()* selectionArray.length);
+  let imageIndex = selectionArray[randomNumber];
+  return imageIndex; //verify use of imageIndex and imageObject usage
+}
+
+// Declarations/Placeholders for three images as per app parameters
+let firstImage;
+let secondImage;
+let thirdImage;
+
+//function called to ensure images are seen once page is initiated
+displayImages()
+
+// declare a function to display images in body of page
+function displayImages(){
+  elImageContainer.innerHTML = '';
+  for(let i=0; i<3; i++){
+    let imageObject = shuffleImage();
+    if(i===0){
+      firstImage = imageObject;
+    }else if (i === 1){
+      while(imageObject.id === firstImage.id){
+        imageObject = shuffleImage();
+      }
+      secondImage = imageObject;
+    }else {
+      while(imageObject.id === firstImage.id || imageObject.id === secondImage.id){
+        imageObject = shuffleImage();
+      }
+      thirdImage = imageObject
+    } 
+    let elImage = document.createElement('img');
+    elImageContainer.appendChild(elImage);
+    elImage.setAttribute('id', imageObject.id);
+    elImage.src = imageObject.filePath;
+    elImage.addEventListener('click', imageClicked);
+    imageObject.shown += 1;
+    if(totalSelected > 24){
+      elImage.removeEventListener('click', imageClicked);
+    } // Display chart function declared below to show chart at the end of eventListener
+    let elCountDown = document.getElementById('selectTracker');
+    elCountDown.innerHTML = 25 - totalSelected;
+  }  
+}
+
+// displayImages()
+//function called after function declaration as well to facilitate image listening functionalities
+
+function startOver(){
+  localStorage.clear();
+  location.reload();
 }
 
 
-let TotalClicked = 0;
-
-//check to verify if local storage exists
-if (localStorage.length > 0) {
-    let getData = localStorage.getItem('storagepictureArray');
-    //     //retrieve stored character Image array from local storage that contains updated clicks and showns
-      pictureArray = JSON.parse(getData);
-    //     //reassign the value of characterImageArray to the parsed version in local storage
-    TotalClicked = localStorage.getItem('TotalClickedStorage');
-
-
-} else {
-    //     //if local storage does not exist, instantiate the constructor to create multiple objects of character images 
-    //instantiate our counstructor to create multiple instances/objects of our character images
-    //generate all 24 image objects as new "ImageConstructor(s) with the name, filePath and id  as properties within the constructor
-    let Disguise = new ImageConstructor('disguise', './assets/dogdisguise.jpg', 'novelty', 'disguise')
-    let Polaroid = new ImageConstructor('polaroid', './assets/polaroidcamera.jpg', 'novelty', 'polaroid')
-    let Typewriter = new ImageConstructor('typewriter', './assets/oldtyper.jpg', 'novelty', 'typewriter')
-    let Vespa = new ImageConstructor('vespa', './assets/vesp.jpg', 'novelty', 'vespa')
-    let Airforce = new ImageConstructor('airforce', './assets/brownairforce.jpg', 'sports', 'airforce')
-    let Basketball = new ImageConstructor('basketball', './assets/nikeball.jpg', 'sports', 'basketball')
-    let Jersey = new ImageConstructor('jersey', './assets/lakersjersey.jpg', 'sports', 'jersey')
-    let Retro = new ImageConstructor('retro', './assets/retrojordan.jpg', 'sports', 'retro')
-    let Soccer = new ImageConstructor('soccer', './assets/nikeboots.jpg', 'sports', 'soccer')
-    let SmokingPipe = new ImageConstructor('SmokingPipe', './assets/smokingpipe.jpg', 'novelty', 'SmokingPipe')
-    let Gear = new ImageConstructor('gear', './assets/sportsbra.jpg', 'sports', 'gear')
-    let Oldbike = new ImageConstructor('oldbike', './assets/oldbike.jpg', 'novelty', 'oldbike')
-    let Spoons = new ImageConstructor('spoon', './assets/spoons.jpg', 'kitchen', 'spoons')
-    let RedKettle = new ImageConstructor('redkettle', './assets/redkettle.jpg', 'kitchen', 'redkettle')
-    let RedVespa = new ImageConstructor('redvespa', './assets/redvespa.jpg', 'novelty', 'redvespa')
-    let Teacup = new ImageConstructor('teacup', './assets/teacup.jpg', 'kitchen', 'teacup')
-    let Hippie = new ImageConstructor('hippie', './assets/hippie.jpg', 'novelty', 'hippie')
-    let Mixbox = new ImageConstructor('mixbowl', './assets/mixbowl.jpg', 'kitchen', 'mixbowl')
-    let Records = new ImageConstructor('records', './assets/oldrecords.jpg', 'novelty', 'records')
-    let Wok = new ImageConstructor('wok', './assets/wok.jpg', 'kitchen', 'wok')
-    let Moped = new ImageConstructor('moped', './assets/yellowmoped.jpg', 'novelty', 'moped')
-    let TypeTwo = new ImageConstructor('typetwo', './assets/vintagetype.jpg', 'novelty', 'typetwo')
-    let Utensils = new ImageConstructor('utensils', './assets/utensils.jpg', 'kitchen', 'utensils')
-    let HippyTwo = new ImageConstructor('hippy2', './assets/hipster.jpg', 'novelty', 'hippy')
-
-    pictureArray.push(Disguise, Polaroid, Typewriter, Vespa, Airforce, Basketball, Jersey, Retro, Soccer, SmokingPipe, Gear, Oldbike, Spoons, RedKettle, RedVespa, Teacup, Hippie, Mixbox, Records, Wok, Moped, TypeTwo, Utensils, HippyTwo)
-
-};
-//ImageArray.category(sports)
-
-function randomImage() {
-    let randomNumber = Math.floor(Math.random() * pictureArray.length);
-    let pictureIndex = pictureArray[randomNumber];
-    pictureIndex.shown += 1
-    return pictureIndex;
+function imageClicked(event){
+  console.log(localStorage);
+  if (event.target.id === firstImage.id){
+    firstImage.selected += 1;
+  }else if(event.target.id === secondImage.id){
+    secondImage.selected += 1;
+  }else if(event.target.id === thirdImage.id){
+    thirdImage.selected += 1;
+  };
+  totalSelected += 1;
+  console.log(totalSelected);
+  displayImages();
+  localStorage.setItem('BusMallStorage', JSON.stringify(selectionArray));
+  localStorage.setItem('TotalSelectedStorage', totalSelected);
+  displayMyChart(); 
 };
 
-displayImages();
-
-// define a function that will display the Random images
-function displayImages() {
-    //reassign the elImageContainer to innerHTML each time the for loop runs
+function displayMyChart(){
+  if(totalSelected > 24){
     elImageContainer.innerHTML = '';
-    for (let i = 0; i < 3; i++) {
-        //declared local variable imageObject and assigned it the randomImage method  which invokes the global variable randomImage() method 
-        let pictureObject = randomImage();//declaration is moved up above the elImage, to prevent conflicts with DOM HTML 
-        //declared local variable elImage an assigned it an img tag in HTML
-        //assign the imageObject to one of the three images in the image container and validate the image
-        //use a conditional statement to validate that current random imageObjects are chosen from the ImageIndex array  
-        if (i === 0) {//moved up to immediately validate that randomImages are populating properly
-            //to populate the images the firstimage must be the imageObject with the index "0" or false
-            firstImage = pictureObject;
-
-            //if ImageObject is assign as index 1 then it can be the secondImage   
-        } else if (i === 1) {
-            //run a while the conition is true generate a new random image if the shown  firstImage category is is equal to the  secondImage id
-            while (pictureObject.id === firstImage.id) {
-                pictureObject = randomImage();
-                console.log('second while', pictureObject.id);
-            }
-            secondImage = pictureObject;
-            //if imageObject is not index 0 or 1, or 2, then it can be used as the thirdImage
-        } else {
-            // run a while the conition is true generate a new random image if the shown thirdImage  is equal to first OR secondImage id
-            while (pictureObject.id === firstImage.id || pictureObject.id === secondImage.id) {
-                pictureObject = randomImage();
-                console.log('third while', pictureObject.id);
-            }
-            thirdImage = pictureObject;
-        };
-
-        let elImage = document.createElement('img');
-        // append elImage to the elImageContainer in HTML via the DOM
-        elImageContainer.appendChild(elImage);
-        //set an id attribute to image element
-        elImage.setAttribute('id', pictureObject.id);
-        //set the image HTML source tag as the images file path 
-        elImage.src = pictureObject.filePath;
-        //added an eventlistener to each image and to store the event within the "click" property of the image variable
-        elImage.addEventListener('click', imageClicked);
-        //increment the shown property  by 1 for each, time the imageObject "shown"   
-        pictureObject.shown += 1;
-    };
-    let elCountDown = document.getElementById('selectTracker')
-        elCountDown.innerHTML = (25 - TotalClicked)
+    displayChart();
+  }
 };
-
-
-//invoke the displayImages function 
-// displayImages();
-
-
-// define even handler function that will increment thetimes clicked for the images
-function imageClicked(event) {
-    // if the id associated with the event target is associatd with firstImage, then  add click event increment by 1
-    if (event.target.id === firstImage.id) {
-        firstImage.clicked += 1;
-        // if the id associated with event target the secondImage.id  is not the firstImage, but is the secondImage, add click event increment by 1
-    } else if (event.target.id === secondImage.id) {
-        secondImage.clicked += 1;
-        // if the id associated with the event target is not firstImage, nor secondImage, but IS associated with thirdImage then add click event increment by 1
-    } else if (event.target.id === thirdImage.id) {
-        thirdImage.clicked += 1;
-    }
-    //prints the event target as the id of the ImageObject
-    console.log('event target', event.target.id);
-    //prints the number of click events in accordance with the associated image
-    console.log('images', firstImage.clicked, secondImage.clicked, thirdImage.clicked);
-
-    //invokes displayImages functions to display 3 new random images after the click event 
-    displayImages();
-    //Total clicks increases by 1 after each click
-    TotalClicked += 1;
-    //print the TotalClicked variable as each click increases
-    console.log(TotalClicked);
-
-    //every time image is clicked, local storage saves character image array.
-    localStorage.setItem('storagepictureArray', JSON.stringify(pictureArray));
-    localStorage.setItem('TotalClickedStorage', TotalClicked);
-    //invoke chart function to display data  for images clicked and shown
-    displayChartNow();
-};
-
-function displayChartNow() {
-    if (TotalClicked > 25) {
-        elImageContainer.removeEventListener('click', imageClicked)
-        displayChart();
-        localStorage.clear();
-    };
-};
-
-
-
-function startOver() {
-    localStorage.clear();
-    location.reload();
-};
-
--- chart.js
+++ chart.js
@@ -1,54 +1,50 @@
-//get canvas elementbyID
+//Select canvas element by ID to commence chart generation
 let elChart = document.getElementById('busChart').getContext('2d');
-//let imported = document.createELement('script')
-//imported.src = './main.js';
-//
-//definee a function that populates chart by looping through Image array
 
-//document.head.appendChild(imported)
+
+//declare a function to populate chart
 function populateChart(property){
-   //assign characterPropsArray as an  empty which will be filled later on
-   let propertyArray = [];
-   for(let i = 0; i < pictureArray.length; i++){
-       propertyArray.push(pictureArray[i][property]);
-      console.log(propertyArray);
-   }
-   //return the
-   return propertyArray;
+  let propertyArrary = [];
+  for(let i = 0; i < selectionArray.length; i++){
+    propertyArrary.push(selectionArray[i][property]);
+  }
+  return propertyArrary;
+}
+
+//declare a function to display chart
+function displayChart(){
+  elChart.innerHTML = '';
+  let busChart = new Chart(elChart, {
+    type: 'bar',
+    data: {
+      labels: populateChart('name'),
+      datasets: [{
+        label: '# of selections',
+        data: populateChart('selected'),
+        
+        backgroundColor: 'blue',
+        borderColor: 'white',
+        borderWidth: 1,
+      },
+      {
+        label: '# of times displayed',
+        data: populateChart('shown'),
+
+        backgroundColor: 'red',
+        borderColor: 'grey',
+        borderWidth: 1,
+      }
+    ]
+    },
+    options: {
+      scales: {
+        yAxis: [{
+          ticks: {
+            beginAtZero:true
+          }
+        }]
+      }
+    }
+  })
 };
 
-
-// declares displayChart variable and assigns
-function displayChart() {
-   elChart.innerHTML = '';
-   let busChart = new Chart(elChart, {
-       //assigns the  type property to become a bar chart's
-       type: 'bar',
-       //assign the data property to contain objects such as subproperties, arrays and functions required to present data on graph and to HTML
-       data: {
-           labels: populateChart('name'),
-           datasets: [
-               {
-                   //click properties associated with the populate first graph that will show the click count for the specific imageObject
-                   label:'Click Count',
-                   data: populateChart('clicked'),
-                   backgroundColor:'blue',
-               },
-               {
-                   label:'Display Count',
-                   data: populateChart('shown'),
-                   backgroundColor: 'red',
-               }
-           ],
-           options: {
-               scales: {
-                   yAxes: [{
-                       ticks: {
-                           beginAtZero:true
-                       }
-                   }]
-               }
-           }
-       }
-   });
-};
